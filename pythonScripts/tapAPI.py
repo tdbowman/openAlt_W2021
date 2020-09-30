f825f752320a0add2ac592ae2472b88689c3bb86
Mitchell Fenner <mitchfen@protonmail.com>
2020-09-30 09:25:26 -0400

    Rollback tapAPI.py - large errors in last commit
    I made changes to this script in an attempt to make ingesting the data easier. The changes I made were not needed and created bugs, so I am rolling it back.

--- pythonScripts/tapAPI.py
+++ pythonScripts/tapAPI.py
@@ -13,20 +13,19 @@ try:
     import schedule
 except ImportError:
     logging.info("You need to install the schedule module using \"pip install schedule\" before proceeding")
-    sys.exit("exit")
-
-# CHANGE THESE IF RESTARTING - CHECK THE LOG
-cursor = "" # Always the first cursor, but you can change it if starting the script from a later point
-fileNameIncrement = 0 # used to save the data files, so that when we sort them for data ingestion, we can restore our progress more easily
+    exit
 
 # DO NOT CHANGE THESE
 email = "mitchfen@protonmail.com"
-source = "twitter" # An empty string will cause it to pull from all sources
+source = "" # An empty string will cause it to pull from all sources
 rows = "10000" # number of Events to pull for today
 fetchURL = "https://api.eventdata.crossref.org/v1/events?mailto="
 tempFileName = "tempFile.json" # file which it writes data to before formatting. Overwritten on each fetchData call
-runOnSchedule = True # False if you want to run it right now, or True if you want it to run on the regular schedule
 logging.basicConfig(filename='tapAPI.log', filemode='a', level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S') # Set the logging parameters
+
+# THESE MAY BE CHANGED
+runOnSchedule = True # False if you want to run it right now, or True if you want it to run on the regular schedule
+cursor = "" # Always the first cursor, but you can change it if starting the script from a later point
 
 def main():
 
@@ -74,21 +73,19 @@ def fetchData():
 def beautifyJSON():
     try:
         global cursor # so that we modify the global version, not this functions copy
-        global fileNameIncrement
         todaysDate = dt.today().strftime("%m-%d-%y--%I-%M") # Semicolons are allowed in Linux, not in Windows Paths
         # This lets us save a file in the same directory as the script if we are debugging
         if (runOnSchedule == True):
-            fileName = "/home/fg7626/crossrefDataDumps/" + fileNameIncrement + "_" + todaysDate + ".json"
+            fileName = "/home/fg7626/crossrefDataDumps/" + todaysDate + ".json"
             #fileName = todaysDate + ".json" # Use this so it writes to the same directory as script when debugging
         else:
-            fileName = fileNameIncrement + "_" + todaysDate + ".json" # Use this so it writes to the same directory as script when debugging
-        fileNameIncrement = fileNameIncrement + 1 # Increment the file number see top of file for description
-        logging.info(" - Beginning JSON formatting - File increment is:" + fileNameIncrement)
+            fileName = todaysDate + ".json" # Use this so it writes to the same directory as script when debugging
+        logging.info(" - Beginning JSON formatting")
         with open(tempFileName) as json_file:
-            data = json.load(json_file) # this is a dict
+            data = json.load(json_file)
             cursor = data.get("message").get("next-cursor")
         os.system("cat " + tempFileName + " | python3 -mjson.tool > " + fileName)
-        logging.info(" - Cursor for next retrieval is: " + str(cursor))
+        logging.info(" - Cursor for this retrieval was " + str(cursor))
         if (cursor == "" or cursor == None):
             logging.info(" - Null cursor - done collecting data - check log for last cursor used")
             sys.exit("exit")
