2746e1d86879ed79196a0b7af34545d7e74f05df
HassanBachir <gf3272@wayne.edu>
2020-11-25 15:19:47 -0500

    Cleaned up code and added comments for code

--- web/landingPageArticles.py
+++ web/landingPageArticles.py
@@ -1,17 +1,29 @@
+#import flask
 import flask
 
+#define function to retrieve the total number of artciles in the database
 def landingPageArticles(mysql):
     #global mysql
     cursor = mysql.connection.cursor()
 
+    #string to query the database to sum up all articles from the _main_ table
     totalSumQuery="SELECT (SELECT COUNT(DISTINCT DOI) FROM dr_bowman_doi_data_tables._main_ ) AS sumCountArticle"
-
+    
+    #execute string's query using cursor
     cursor.execute(totalSumQuery)
+    
+    #commit
     mysql.connection.commit()
+    
+    #fetch result
     totalSumArticles = cursor.fetchone()
-
+    
+    #print result in terminal for testing purposes
     print(totalSumArticles)
 
+    #close cursor
     cursor.close()
+    
+    #return result fetched from the dictionary
     return (totalSumArticles['sumCountArticle'])
     
