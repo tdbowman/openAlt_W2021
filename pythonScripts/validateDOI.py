d7df33e00e656bf45252a496cb0244d57fd27716
Salsabil Bakth <ft2054@wayne.edu>
2021-01-26 23:24:40 -0500

    Validate DOI inputs
    Differentiate between URL, DOI ID, and DOI number

--- pythonScripts/validateDOI.py
+++ pythonScripts/validateDOI.py
@@ -3,14 +3,41 @@
 ## url = "http://dx.doi.org/10.1192/bjp.171.6.519"
 
 fields = []
-
-urlSplit = "http://dx.doi.org/"
 IDSplit = "doi:"
+urlSplit = "http://dx.doi.org/"
 
 file = open("ListDOI.txt","r")
 
 for line in file:
-    fields = line.split(urlSplit)
-    print(fields[1])
+    ##fields = line.split(IDSplit)
+
+    if(file.read(4) == "doi:"):
+        fields = line.split(IDSplit)
+    elif(file.read(18) == "http://dx.doi.org/"):
+        fields = line.split(urlSplit)
+    elif(file.read(3) == "10."):
+        fields = line
+    else:
+        print("Error retrieving DOI.")
+
+    try:
+        from crossref.restful import Works
+        works = Works()
+
+        # Request data from the crossref API, save json as x
+        x = works.doi(fields[1])
+        if (x['author']):
+            authorList = x['author']
+            for index, authorDetail in enumerate(authorList):
+                first_name = authorDetail['given']
+                last_name = authorDetail['family']
+                print(first_name + ' ' + last_name)
+                
+    except ImportError:
+        print("You need to install the crossref api with 'pip install crossrefapi' first")
+    except:
+        print("Unspecified error")
+
 file.close()
+
 
