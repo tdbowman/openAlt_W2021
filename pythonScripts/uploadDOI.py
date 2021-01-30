2460624b1e8567814254981471fee31bfba525e4
darpanshah-wsu <darpan.shah@wayne.edu>
2021-01-30 16:13:45 -0500

    uploadDOI.py now parses DOI value based on DOI formats given in config_doi.txt

--- pythonScripts/uploadDOI.py
+++ pythonScripts/uploadDOI.py
@@ -2,9 +2,8 @@ import csv
 import pandas
 import logging
 
-### Start of Darpan's Work ###
 
-#Set the logging parameters
+# Set the logging parameters
 logging.basicConfig(filename='./doi_upload.log', filemode='a', level=logging.INFO,
     format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')  
 
@@ -14,59 +13,65 @@ except:
     print("MySQL Connector Exception")
     logging.info("Cannot determine how you intend to run the program")
 
-#directory of doi list
-#CHANGE DIRECTORY TO YOUR DOI LIST CSV
-dir = 'C:\\Users\\darpa\\Desktop\\openAlt_W2021\\pythonScripts\\template_doi.csv'
+# directories
+# CHANGE DIRECTORY TO YOUR DOI LIST CSV AND CONFIG FILE
+dir_template = 'C:\\Users\\darpa\\Desktop\\openAlt_W2021\\pythonScripts\\template_doi.csv'
+dir_config = 'C:\\Users\\darpa\\Desktop\\openAlt_W2021\\pythonScripts\\config_doi.txt'
+
 
 doi_arr = []
 
-#pandas library reads doi list
-doi_list = pandas.read_csv(dir, header=None)
+# pandas library reads doi list
+doi_list = pandas.read_csv(dir_template, header=None)
 
 
-#adds doi values into array and prints the array
+# adds doi values into array and prints the array
 for x in range(len(doi_list)):
     doi_arr.append(doi_list.values[x][0])
 
 
-#print(doi_arr)
-
-## End of Darpan's Work
+# print(doi_arr)
 
 
-## Start of Salsabil's Work ##
+# Reading config file to parse doi input to only include number
+config_arr = []
+config_file = open(dir_config,'r')
 
-# Parsing doi input to only include number
-doi = "doi:"
-url = "http://dx.doi.org/"    
+# Reading config file line by line
+# Without the r strip, '\n' is added on to the value -> Ex. 'doi:\n'
+for line in config_file:
+    config_arr.append(line.rstrip('\n'))  
 
-for values in doi_arr:
-    values = values.replace(doi,'')
-    values = values.replace(url,'')
-    #values = "\'" + values + "\'"
+# print('\nCONFIG ARR:',config_arr)
+# print('\nDOI ARR:', doi_arr)
+
+
+# Parse out doi formatting
+for config in config_arr:
+    doi_arr = [doi.replace(config,'') for doi in doi_arr]
+
+
+#print("\nPARSED DOI ARR:", doi_arr)
     
 
 joinedArr = "\'" + "','".join(doi_arr) + "\'"
-print(joinedArr)
+#print("\nJOINED ARRAY:",joinedArr)
 print("\n")
 
-#Getting MySQL database credentials
+# Getting MySQL database credentials
 print("\nMySQL Credentials")
 mysql_username = input("Username: ")
 mysql_password = input("Password: ")
 
-#Connecting to database
+# Connecting to database
 connection = mysql.connector.connect(user=str(mysql_username), password=str(
         mysql_password), host='127.0.0.1', database='crossrefeventdatamain')
 
 cursor = connection.cursor()  
 
-## End of Salsabil's Work ##
 
-
-## Start of Darpan's Work
-#Execution of query and output of result + log
-query = 'SELECT DOI FROM dr_bowman_doi_ data_tables._main_ WHERE DOI IN (' + joinedArr + ');'
+# Execution of query and output of result + log
+query = 'SELECT DOI FROM dr_bowman_doi_data_tables._main_ WHERE DOI IN (' + joinedArr + ');'
 
 print('\n',query)
 logging.info(query)
@@ -75,7 +80,7 @@ logging.info(cursor.fetchall())
 cursor.execute(query)
 print(cursor.fetchall())
 
-## End of Darpan's Work
+
 
 
 
