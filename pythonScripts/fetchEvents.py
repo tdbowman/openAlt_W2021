aaa41ee92b8c632bbf41e4da47d85167bfc8bae1
Rihat Rahman <gi3345@wayne.edu>
2021-02-06 12:52:18 -0500

    Simplified code by removing unnecessary lines and added functionality to make integration easier

--- pythonScripts/fetchEvents.py
+++ pythonScripts/fetchEvents.py
@@ -1,72 +1,85 @@
 # Author: Rihat Rahman
+# This scripts fetches events for papers from dr_bowman_doi_data_tables database and stores event information 
+# in JSON files. It also creates a CSV filecontaining information about which JSON file belongs to what DOI.
+# After running this script, change dataDirectory of IngestJSONMain.py to eventData folder and run that script 
+# to ingest data into crossrefevent database
 
 import mysql.connector
 import os
 import platform
 import csv
 
-mysql_username = "root"
-mysql_password = "Dsus1209."
 
-# print("MySQL Credentials")
-# mysql_username = input("Username: ")
-# mysql_password = input("Password: ")
+def fetch_events():
 
-# connect to dr_bowman_doi_data_tables database
-drBowmanDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "dr_bowman_doi_data_tables")
-
-# connect to crossrefeventdatamain database
-eventDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "crossrefeventdatamain")
-
-drBowmanDatabaseCursor = drBowmanDatabase.cursor()
-eventDatabaseCursor = eventDatabase.cursor()
-
-drBowmanDatabaseCursor.execute("Select DOI FROM _main_")
-articles = drBowmanDatabaseCursor.fetchall()
-
-# To fetch event data for all articles, set numberOfArticlesToFetch = articles.length
-numberOfArticlesToFetch = 500 
-
-# create directory to store temporary JSON files
-eventDataDirectory = 'eventData'
-
-emptyDOI = (None, )
+    print("MySQL Credentials")
+    mysql_username = input("Username: ")
+    mysql_password = input("Password: ")
 
 
-if platform.system() == "Windows":
-    directory = "eventData\\events_"
-
-elif platform.system() == "Darwin" | "Linux":
-    "eventData/events_"
-
-if not os.path.exists("eventData"):
-    os.makedirs(eventDataDirectory)
+    try:
+        # connect to dr_bowman_doi_data_tables database
+        drBowmanDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "dr_bowman_doi_data_tables")
 
 
-with open("json_info.csv", "w", newline="") as jsonInfoFile:
-    writer = csv.writer(jsonInfoFile)
-    writer.writerow(["File Name", "DOI"])
+    except:
+        print("Error: Invalid MySQL credentials")
+        return
 
-    fileCounter = 1
+    print ("Connected to the database...")
+
+    drBowmanDatabaseCursor = drBowmanDatabase.cursor()
+
+    drBowmanDatabaseCursor.execute("Select DOI FROM _main_")
+    articles = drBowmanDatabaseCursor.fetchall()
+
+    # To fetch event data for all articles, set numberOfArticlesToFetch = articles.length
+    numberOfArticlesToFetch = 500 
+
+    # create directory to store temporary JSON files
+    eventDataDirectory = 'eventData'
+
+    emptyDOI = (None, )
 
 
-    for i in range(numberOfArticlesToFetch):
+    # path for different operating systems
+    if platform.system() == "Windows":
+        directory = "eventData\\events_"
 
-        if (articles[i] != emptyDOI):
+    elif platform.system() == "Darwin" | "Linux":
+        "eventData/events_"
 
-            article = articles[i]
-            articleDOI = article[0]
+    if not os.path.exists("eventData"):
+        os.makedirs(eventDataDirectory)
 
-            print(articleDOI)
 
-            fileName = directory + str(fileCounter) + ".json"
-            fileCounter += 1
-            print(fileName)
+    with open("json_info.csv", "w", newline="") as jsonInfoFile:
+        writer = csv.writer(jsonInfoFile)
+        writer.writerow(["File Name", "DOI"])
 
-            query = "curl " + "\"" + "https://api.eventdata.crossref.org/v1/events?mailto=YOUR_EMAIL_HERE&obj-id=" + articleDOI + "\"" + " > " + fileName
-            os.system(query)
+        fileCounter = 1
 
-            writer.writerow([fileName[10:], articleDOI])
 
-            eventDatabaseCursor.execute("DELETE FROM crossrefeventdatamain.main WHERE objectID = 'https://doi.org/" + articleDOI + "';")
-            eventDatabase.commit()
+        for i in range(numberOfArticlesToFetch):
+
+            if (articles[i] != emptyDOI):
+
+                article = articles[i]
+                articleDOI = article[0]
+
+                print(articleDOI)
+
+                fileName = directory + str(fileCounter) + ".json"
+                fileCounter += 1
+                print(fileName)
+
+                # fetching event data for this particular DOI
+                query = "curl " + "\"" + "https://api.eventdata.crossref.org/v1/events?mailto=YOUR_EMAIL_HERE&obj-id=" + articleDOI + "\"" + " > " + fileName
+                os.system(query)
+
+                writer.writerow([fileName[10:], articleDOI])
+
+
+
+if __name__ == '__main__':
+    fetch_events()
