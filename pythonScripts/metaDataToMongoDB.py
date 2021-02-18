ed0aa95d95758a62bdfbfd03e9b0ccb143548c68
shaikhta <77861065+shaikhta@users.noreply.github.com>
2021-02-18 11:10:06 -0500

    Update metaDataToMongoDB.py

--- pythonScripts/metaDataToMongoDB.py
+++ pythonScripts/metaDataToMongoDB.py
@@ -4,36 +4,28 @@ import pandas
 import json
 import requests
 import mysql.connector
-
+import urllib
 client=pymongo.MongoClient('mongodb://localhost:27017/')
 
 def fetch():
     print("MySQL Credentials")
     mysql_username = "root"
     mysql_password = "pass"
-
-
     try:
         drBowmanDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "dr_bowman_doi_data_tables")
-
-
     except:
         print("Error: Invalid MySQL credentials")
         return
-
     print ("Connected to the database...")
-    drBowmanDatabaseCursor = drBowmanDatabase.cursor()
-
-    drBowmanDatabaseCursor.execute("Select DOI FROM _main_ WHERE DOI IS NOT NULL")
-
-    articles = drBowmanDatabaseCursor.fetchall()
-    print(articles)
 
 def main():
-    dbs=client.list_database_names()
-    if client==pymongo.MongoClient('mongodb://localhost:27017/'):
+    dbs=client.local
+    if dbs==client.local:
         print(dbs)
-
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('Accept', 'application/vnd.crossref.unixsd+xml')]
+    r = opener.open('http://dx.doi.org/10.5555/515151')
+    print (r.info()['Link'])
 
 
 if __name__=='__main__':
