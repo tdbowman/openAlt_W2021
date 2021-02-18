be6b098e401b1095bc20df3067d59bd40e2fb475
Rihat Rahman <gi3345@wayne.edu>
2021-02-18 00:04:41 -0500

    Script to fetch all citation and reference data and store them on MongoDB buffer database

--- pythonScripts/fetchOpencitationsToMongoDB.py
+++ pythonScripts/fetchOpencitationsToMongoDB.py
@@ -1,25 +1,109 @@
-# inserts a sample from OpenCitations into MongoDB
-# author: Rihat Rahman
-# Lines 1-24
-#-------------------------------------------------------------
+import json
 import requests
 import pymongo
+import mysql.connector
 
-# connect to MongoDB
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
-# create database
-citationsDatabase = myclient["cDatabase"]
+def fetchDOICitations ():
+    # mysql credentials
+    mysql_username ='root'
+    mysql_password = 'Dsus1209.'
 
-# create collections
-citations = citationsDatabase["citations"]
 
-# get sample data from OpenCitations
-response = requests.get('https://w3id.org/oc/index/api/v1/citations/10.1002/adfm.201505328')
+    drBowmanDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "dr_bowman_doi_data_tables")
+    openCitationsDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "opencitations")
 
-# insert data
-insertion = citations.insert_many(response.json())
+    drBowmanDatabaseCursor = drBowmanDatabase.cursor()
+    openCitationsCursor = openCitationsDatabase.cursor()
 
-# print id's of documents that were inserted
-print(insertion.inserted_ids)
-#-------------------------------------------------------------
+    drBowmanDatabaseCursor.execute("Select DOI FROM _main_ WHERE DOI IS NOT NULL")
+    articles = drBowmanDatabaseCursor.fetchall()
+
+    for article in articles:
+        fetchCitationData(article[0], openCitationsCursor)
+
+
+
+def fetchCitationData (doi, openCitationsCursor):
+
+    # connect to MongoDB
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+    # database
+    citationsDatabase = myclient["cDatabase"]
+
+    # collections
+    citationCollections = citationsDatabase["citations"]
+    citationCountCollections = citationsDatabase["citation_counts"]
+    referenceCollections = citationsDatabase["references"]
+    referenceCountsCollections = citationsDatabase["reference_counts"]
+
+
+
+
+
+    # get list of citations that already exist in MySQL
+    openCitationsCursor.execute("Select oci FROM citations WHERE cited = '" + doi + "'")
+    citationsOCI = openCitationsCursor.fetchall()
+
+
+    listOfOCIs = []
+
+    for oci in citationsOCI:
+        listOfOCIs.append(oci[0])
+
+    # citations (list of publications that cited this DOI)
+    doi = '10.1002/adfm.201505328'
+    citationResponse = requests.get('https://w3id.org/oc/index/api/v1/citations/' + doi)
+    citationsJSON = citationResponse.json()
+    print(citationsJSON)
+
+    # check if any of the fetched citations already exist in MySQL
+    # If not insert into MongoDB
+    for citation in citationsJSON:
+        if citation['oci'] in listOfOCIs:
+            citationsJSON.remove(citation)
+
+    citationCollections.insert_many(citationsJSON)
+
+
+
+    # citation count (total number of publications that cited this DOI)
+    citationCountsResponse = requests.get('https://opencitations.net/index/croci/api/v1/citation-count/' + doi)
+    citationCountsJSON = citationCountsResponse.json()
+
+
+
+    openCitationsCursor.execute("Select oci FROM ref WHERE citing = '" + doi + "'")
+    referencesOCI = openCitationsCursor.fetchall()
+
+    # references (list of publications referenced by this DOI)
+    referenceResponse = requests.get('https://w3id.org/oc/index/api/v1/references/' + doi)
+    referencesJSON = referenceResponse.json()
+
+    for reference in referencesJSON:
+        if reference['oci'] in referencesOCI:
+            referencesJSON.remove(reference)
+
+    referenceCollections.insert_many(referencesJSON)
+    
+
+
+    # reference count (total number of publications referenced by this DOI)
+    referenceCountsResponse = requests.get('https://opencitations.net/index/croci/api/v1/reference-count/' + doi)
+    referenceCountsJson = referenceCountsResponse.json()
+            
+
+
+if __name__ == '__main__':
+    
+    # mysql credentials
+    mysql_username ='root'
+    mysql_password = 'Dsus1209.'
+
+
+    openCitationsDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "opencitations")
+
+    openCitationsCursor = openCitationsDatabase.cursor()
+
+    fetchCitationData('10.1002/adfm.201505328', openCitationsCursor)
