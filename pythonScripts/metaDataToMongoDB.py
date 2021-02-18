1bfd7a2ddcd32676bba8b5742f9b943ba1f4b1dc
shaikhta <77861065+shaikhta@users.noreply.github.com>
2021-02-18 12:14:01 -0500

    Update metaDataToMongoDB.py

--- pythonScripts/metaDataToMongoDB.py
+++ pythonScripts/metaDataToMongoDB.py
@@ -1,33 +1,34 @@
+# Tabish's work
 import pymongo
 import os
 import pandas
 import json
 import requests
 import mysql.connector
-import urllib
-client=pymongo.MongoClient('mongodb://localhost:27017/')
 
-def fetch():
+#client=pymongo.MongoClient()
+
+def connecttomysql():
     print("MySQL Credentials")
     mysql_username = "root"
     mysql_password = "pass"
-    try:
-        drBowmanDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "dr_bowman_doi_data_tables")
-    except:
-        print("Error: Invalid MySQL credentials")
-        return
-    print ("Connected to the database...")
+    drBowmanDatabase = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "dr_bowman_doi_data_tables")
+    print ("Connected to the database")
 
 def main():
+    try:
+        client = pymongo.MongoClient('mongodb://localhost:27017/')
+        print("Connected successfully to MongoDB")
+    except:
+        print("Could not connect to MongoDB")
+        return
     dbs=client.local
-    if dbs==client.local:
-        print(dbs)
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('Accept', 'application/vnd.crossref.unixsd+xml')]
-    r = opener.open('http://dx.doi.org/10.5555/515151')
-    print (r.info()['Link'])
-
+    coll=dbs.my_coll
+    r = requests.get('https://api.crossref.org/v1/works/10.1088/0004-637X/722/2/971')
+    id1 = coll.insert_one(r.json())
+    print(id1)
+    print(coll)
 
 if __name__=='__main__':
-    fetch()
+    #connecttomysql()
     main()
