4b11c3791c1de1fd8051883d7302d2c76bd5c9b5
shaikhta <77861065+shaikhta@users.noreply.github.com>
2021-02-21 07:53:19 -0500

    Updates

--- pythonScripts/metaDataToMongoDB.py
+++ pythonScripts/metaDataToMongoDB.py
@@ -4,20 +4,25 @@ import os
 import requests
 
 def storeMetaDatainMongoDB():
+    # hard code for testing; change later
+    # retrieve metadata from api
     r = requests.get('https://api.crossref.org/works?sample=10')
-
+    # connect to localhost MongoDB
     try:
         client = pymongo.MongoClient('mongodb://localhost:27017/')
-        print("Connected successfully to MongoDB")
     except:
         print("Could not connect to MongoDB")
         return
-
-    dbs=client.retrievedFromAPI
-
-    coll=dbs.my_coll
-
-    coll.insert_one(r.json())
+    # cursor to spcified database; create if it doesn't exist
+    dbs=client["MetadataDatabase"]
+    # cursor to specified collection; create if it doesn't exist
+    md=dbs["MetaData"]
+    # store metadate from api as json
+    data=r.json()
+    # find relevant data in nested dictionary
+    data=data.get("message").get("items")
+    # store data in collection
+    md.insert_many(data)
 
 if __name__=='__main__':
     storeMetaDatainMongoDB()
