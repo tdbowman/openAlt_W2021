4e6b0c03368755c1b3086602ea75fe62a6c09ace
shaikhta <77861065+shaikhta@users.noreply.github.com>
2021-02-24 20:01:42 -0500

    Updates
    Finished retrieval and duplicate checking, still working on insertion

--- pythonScripts/metaDataToMongoDB.py
+++ pythonScripts/metaDataToMongoDB.py
@@ -1,12 +1,12 @@
 # Tabish's work
 import pymongo
-import os
 import requests
 
 def storeMetaDatainMongoDB():
     # hard code for testing; change later
     # retrieve metadata from api
-    r = requests.get('https://api.crossref.org/works?sample=10')
+    r = requests.get('https://api.crossref.org/works?sample=5&mailto=tabishshaikh97@gmail.com')
+    q = requests.get('https://api.crossref.org/works/10.1192/bjp.171.6.519')
     # connect to localhost MongoDB
     try:
         client = pymongo.MongoClient('mongodb://localhost:27017/')
@@ -18,11 +18,10 @@ def storeMetaDatainMongoDB():
     # cursor to specified collection; create if it doesn't exist
     md=dbs["MetaData"]
     # store metadate from api as json
-    data=r.json()
-    # find relevant data in nested dictionary
-    data=data.get("message").get("items")
-    # store data in collection
-    md.insert_many(data)
+    data1=r.json()
+    md.insert(data1)
+    data2=q.json()
+    md.insert(data2)
 
 if __name__=='__main__':
     storeMetaDatainMongoDB()
--- pythonScripts/mongoDBtoMySQL.py
+++ pythonScripts/mongoDBtoMySQL.py
@@ -5,20 +5,25 @@ import pandas
 import json
 import requests
 import mysql.connector
+from metaDataToMongoDB import storeMetaDatainMongoDB
 
 def retrieveFromMongoDB():
-    try:
-        client = pymongo.MongoClient('mongodb://localhost:27017/')
-    except:
-        print("Could not connect to MongoDB")
-        return
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
     # cursor to spcified database; create if it doesn't exist
-    dbs=client["MetadataDatabase"]
+    db=client["MetadataDatabase"]
     # cursor to specified collection; create if it doesn't exist
-    md=dbs["MetaData"]
-    storeInMySQL(md)
+    coll=db["MetaData"]
+    data=coll.find({},{"_id":1,"message-type":1,"message":1})
+    for i in data:
+        print(i.get("_id"))
+        if i.get("message-type")=="work-list":
+            checkmysqlforlist(i.get("message").get("items"))
+        elif i.get("message-type")=="work":
+            checkmysqlfordict(i.get("message"))
+        else:
+            print("Invalid data")
 
-def storeInMySQL(md):
+def checkmysqlforlist(li):
     mysql_username = "root"
     mysql_password = "pass"
 
@@ -31,9 +36,63 @@ def storeInMySQL(md):
         print("Could not connect to MySQL")
         return
     myCursor=drBowmanDatabase.cursor()
-    sql = "INSERT INTO _main_ VALUES " + md
+    myCursor.execute("SELECT DOI FROM _metadata_")
+    dupe=[False]*len(li)
+    for i in myCursor.fetchall():
+        for j in li:
+            if i[0]==j.get("DOI"):
+                print("found dupe")
+                dupe[j]=True
+    for j in dupe:
+        if dupe[j]==False:
+            print("no dice")
+            storeinmysql(li[j])
 
-    #drBowmanDatabase.commit()
+def checkmysqlfordict(di):
+    mysql_username = "root"
+    mysql_password = "pass"
+    try:
+        drBowmanDatabase = mysql.connector.connect( host = "localhost",
+                                                    user = mysql_username,
+                                                    passwd = mysql_password,
+                                                    database = "dr_bowman_doi_data_tables")
+    except:
+        print("Could not connect to MySQL")
+        return
+    myCursor=drBowmanDatabase.cursor()
+    myCursor.execute("SELECT DOI FROM _metadata_")
+    dupe=False
+    for i in myCursor.fetchall():
+        if i[0] == di.get("DOI"):
+            print("found dupe")
+            dupe=True
+    if dupe==False:
+        print("no dice")
+        storeinmysql(di)
+
+def storeinmysql(data):
+    mysql_username = "root"
+    mysql_password = "pass"
+    try:
+        drBowmanDatabase = mysql.connector.connect( host = "localhost",
+                                                    user = mysql_username,
+                                                    passwd = mysql_password,
+                                                    database = "dr_bowman_doi_data_tables")
+    except:
+        print("Could not connect to MySQL")
+        return
+    myCursor=drBowmanDatabase.cursor()
+    df=pandas.DataFrame(data)
+    print(type(df))
+    #myCursor.execute("INSERT INTO _test_ VALUES %s", json_object)
+
+
+
+
+
+
+    return
 
 if __name__=='__main__':
+    #storeMetaDatainMongoDB()
     retrieveFromMongoDB()
