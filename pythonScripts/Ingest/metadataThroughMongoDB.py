5c4881b4c88a90083d2724054cf25c3b1d67e4f9
shaikhta <77861065+shaikhta@users.noreply.github.com>
2021-03-30 13:47:57 -0400

    Updating metadata ingest
    still need to get country and university columns filled

--- pythonScripts/Ingest/metadataThroughMongoDB.py
+++ pythonScripts/Ingest/metadataThroughMongoDB.py
@@ -1,59 +1,59 @@
 # Tabish's work
 import pymongo
 import os
+import sys
 import pandas
 import json
 import requests
 import mysql.connector
 from ingestCrossrefMetadata import crossrefMetadataIngest
 
-def hashmap():
-    listofnames = {}
-    # Connect to MySQL database
+def hashmap(listoffks):
     mysql_username = "root"
     mysql_password = "pass"
     try:
         db = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "doidata")
     except:
-        print("Could not connect to MySQL")
+        print("Could not connect to MySQL.")
         return
     myCursor=db.cursor(buffered=True)
-    myCursor.execute("SELECT name FROM author")
-    # Fetch all rows of column name from the author table
-    nms = myCursor.fetchall()
-    # Store all names in a dictionary
-    for i in nms:
-        listofnames[i[0]]=None
-    # Return dictionary
-    return listofnames
+    myCursor.execute("SELECT DOI,fk FROM _main_ WHERE DOI IS NOT NULL")
+    fks=myCursor.fetchall()
+    for i,j in fks:
+        listoffks[i]=j
+    return listoffks
 
-def storeMetaDatainMongoDB(listofnames, DOI):
+def storeMetaDatainMongoDB(DOI, fk):
     # retrieve metadata from api
     r = requests.get('https://api.crossref.org/works/'+ DOI)
     # connect to localhost MongoDB
     try:
         client = pymongo.MongoClient('mongodb://localhost:27017/')
     except:
-        print("Could not connect to MongoDB")
+        print("Could not connect to MongoDB.")
         return
     # cursor to spcified database; create if it doesn't exist
     dbs=client["MetadataDatabase"]
     # cursor to specified collection; create if it doesn't exist
     coll=dbs["MetaData"]
+    # Make sure collection is empty
+    coll.delete_many({})
+    # Try to convert httpresponse to JSON
     try:
          data=r.json()
     except:
-        print("Invalid data")
+        print("Invalid data. Data could not be converted to JSON.")
         return
+    # Insert JSON data in MongoDB
     if data.get("message-type")=="work":
         coll.insert_one(data.get("message"))
     else:
-        print("Invalid data")
+        print("Invalid data. Use message-type = work.")
         return
-    storeinmysql(listofnames, DOI, coll)
+    storeinmysql(coll, DOI, fk)
     return
 
-def storeinmysql(listofnames, DOI, coll):
+def storeinmysql(coll, DOI, fk):
     # Calls function from ingestCrossrefMetadata.py
     # and stores metadata in MySQL
     mysql_username = "root"
@@ -61,30 +61,31 @@ def storeinmysql(listofnames, DOI, coll):
     try:
         db = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "doidata")
     except:
-        print("Could not connect to MySQL")
+        print("Could not connect to MySQL.")
         return
     myCursor=db.cursor(buffered=True)
-    myCursor.execute("SELECT * FROM author")
+    #myCursor.execute("SELECT * FROM author")
     for data in coll.find({},{"author":1}):
-        if "author" in data:
-            crossrefMetadataIngest(data["author"], myCursor, db, listofnames)
+        if "author" in data.keys():
+            for i in data["author"]:
+                crossrefMetadataIngest(i, myCursor, db, fk)
+        else:
+            print("Invalid data. Author data not found.")
+            return
     # End the connection to the MySQL database
     myCursor.close()
     db.close()
-    print("Data stored")
-    # Delete all contents of collection; delete_many used just to be safe
+    print("Data stored.")
+    # Delete all contents of collection
     coll.delete_many({})
     return
 
+def main():
+    listoffks = hashmap({})
+    for key,value in listoffks.items():
+        print(value)
+        storeMetaDatainMongoDB(key, value)
+
 if __name__=='__main__':
-    # This main block is only for testing; implementation instructions below
-    # First call hashmap() to create first argument for the storeMetaDatainMongoDB() function
-    # Next call storeMetaDatainMongoDB() function with the dictionary from hashmap() and the DOI as arguments
-    mysql_username = "root"
-    mysql_password = "pass"
-    db = mysql.connector.connect(host = "localhost", user = mysql_username, passwd = mysql_password, database = "doidata")
-    myCursor=db.cursor(buffered=True)
-    myCursor.execute("SELECT DOI FROM _main_")
-    listofnames = hashmap()
-    for i in myCursor:
-        storeMetaDatainMongoDB(listofnames, str(i[0]))
+    # This main block is only for testing
+    main()
