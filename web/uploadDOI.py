133ae0a6364d7ae4286ae49d1d9c1f48d9e9933d
darpanshah-wsu <darpan.shah@wayne.edu>
2021-04-23 15:24:04 -0400

    updated config

--- web/uploadDOI.py
+++ web/uploadDOI.py
@@ -41,7 +41,7 @@ import emailResults as er
 path = os.getcwd() 
 parent = os.path.dirname(path) 
 #config_path = os.path.join(path, "config", "openAltConfig.json")
-config_path = "C:\\Users\\salsa\\Documents\\GitHub\\openAlt_W2021\\config\\openAltConfig.json"
+config_path = "C:\\Users\\darpa\\Desktop\\openAlt_W2021\\config\\openAltConfig.json"
 f = open(config_path)
 
 APP_CONFIG = json.load(f)
